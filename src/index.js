073e19ad84f02bbe3d8bd8cbbe8273ad14c34ac5
Tyler <tyler.hagstrom@gmail.com>
2021-09-30 13:26:13 -0500

    Added a function to be able to add a project, as well as if a ToDo object is created with a non-existing project it will create the project automatically

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import todoObject from './todoitems.js';
 import './sub.js';
 
 const item1 = new todoObject("project", "Odin project", "tomorrow", 1, "in progress", 'default');
-new todoObject("project", "Odin project", "tomorrow", 1, "in progress", 'default');
+new todoObject("project", "Odin project", "tomorrow", 1, "in progress", 'TheOdin');
--- src/storage.js
+++ src/storage.js
@@ -8,18 +8,37 @@ let projects = {
 const addToArray = function (msg, data){
     switch(msg){
         case 'toDoAdded':
-            const project = data.project;
-            projects[project].push(data);
+            addToDotoProject(data.project, data);
             console.log(projects);
             break;
         case 'projectAdded':
-            projects[data] = [];
+            addToProject(data);
             console.log(data);
             break;
     }
 }
 
 const doesProjectExist = function(project){
+    const keys = Object.keys(projects);
+    if((keys.indexOf(project)) != -1){
+        return true;
+    }
+    return false;
+}
+
+const addToProject = function(project){
+    if (doesProjectExist(project) === true){
+        return "Project already exists";
+    } else {
+        projects[project] = [];
+    }
+}
+
+const addToDotoProject = function(project, todo){
+    if (doesProjectExist(project) === false){
+        addToProject(project);}
+
+    projects[project].push(todo);
 
 }
 
