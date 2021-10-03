c5a52226e33b20d87430a401b6aaa4d31d4dfc65
Tyler <tyler.hagstrom@gmail.com>
2021-10-03 15:00:21 -0500

    Fixed up the merge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {toDoObject} from "./todoObject";
 import './styles.css';
 import './PubSub.js'
@@ -10,18 +9,3 @@ item2.display();
 
 
 PubSub.publish('pageLoaded', null);
-=======
-import {todoObject} from './todoitems.js';
-import './sub.js';
-import PubSub from 'pubsub-js';
-import './styles.css';
-
-//const item1 = new todoObject("project", "Odin project", "tomorrow", 1, "in progress", 'default');
-//new todoObject("project", "Odin project", "tomorrow", 1, "Finished", 'default');
-
-const projectSubmitButton = document.querySelector('#addProjectButton');
-projectSubmitButton.addEventListener('click', function(){PubSub.publish('projectAdded', document.querySelector("#newProject").value)});
-const ToDoSubmitButton = document.querySelector('#addToDoButton');
-ToDoSubmitButton.addEventListener('click', function(){PubSub.publish('ToDoAddedFromUI')});
-PubSub.publish('pageLoaded');
->>>>>>> refs/remotes/origin/main
